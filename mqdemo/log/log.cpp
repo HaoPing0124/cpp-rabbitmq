cf546212830c3f9894b99bd859e2c93bd075e5df
Hao_Ping <2429302207@qq.com>
2026-05-30 20:23:06 +0800

    日志输出工具宏实现

---
+++ mqdemo/log/log.cpp
@@ -0,0 +1,38 @@
+/*
+    日志宏 通过日志宏进行日志打印 在打印的信息前需带有系统时间以及文件名和行号
+    格式：[2026-05-30 20:20:12] [DEBUG] [4009556] [log.cpp:34] - Hello World
+*/
+
+#include <iostream>
+#include <ctime>
+#include <unistd.h>
+
+#define DEBUG_LEVEL 0
+#define INFO_LEVEL 1
+#define ERROR_LEVEL 2
+
+#define DEFAULT_LEVEL DEBUG_LEVEL
+
+#define LOG(lev_str, level, format, ...) {\
+    if(level >= DEFAULT_LEVEL)\
+    {\
+        time_t t = time(nullptr);\
+        struct tm* ptm = localtime(&t);\
+        char time_str[32];\
+        strftime(time_str, sizeof(time_str) - 1, "%Y-%m-%d %H:%M:%S", ptm);\
+        pid_t pid = getpid();\
+        printf("[%s] [%s] [%ld] [%s:%d] - " format "\n", time_str, lev_str, (long)pid , __FILE__, __LINE__, ##__VA_ARGS__);\
+    }\
+}
+
+#define DLOG(format, ...) LOG("DEBUG", DEBUG_LEVEL, format, ##__VA_ARGS__)
+#define ILOG(format, ...) LOG("INFO", INFO_LEVEL, format, ##__VA_ARGS__)
+#define ELOG(format, ...) LOG("ERROR", ERROR_LEVEL, format, ##__VA_ARGS__)
+
+int main()
+{
+    DLOG("Hello World");
+    ILOG("Hello World");
+    ELOG("Hello World");
+    return 0;
+}
