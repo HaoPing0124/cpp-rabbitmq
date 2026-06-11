a85aa5f47ca2dd3bda9c0aecfa19bcf9d3c18d1a
Hao_Ping <2429302207@qq.com>
2026-06-12 01:35:15 +0800

    持久化信息管理类实现

--- mqserver/mq_binding.hpp
+++ mqserver/mq_binding.hpp
@@ -16,14 +16,15 @@ namespace haoping
     struct Binding
     {
         using ptr = std::shared_ptr<Binding>;
-        std::string exchange_name;  // 交换机名称
-        std::string msgqueue_name;  // 队列名称
-        std::string binding_key;    // 绑定信息
+        std::string exchange_name; // 交换机名称
+        std::string msgqueue_name; // 队列名称
+        std::string binding_key;   // 绑定信息
 
-        Binding(){}
+        Binding() {}
         Binding(const std::string &ename, const std::string &qname, const std::string &key)
-            :exchange_name(ename), msgqueue_name(qname), binding_key(key)
-        {}
+            : exchange_name(ename), msgqueue_name(qname), binding_key(key)
+        {
+        }
     };
 
     // 队列与绑定信息是一一对应的(因为是给某个交换机去绑定队列，因此一个交换机可能有多个队列的绑定信息)
@@ -40,28 +41,97 @@ namespace haoping
     class BindingMapper
     {
     public:
-        BindingMapper(const std::string &dbfile) :_sql_helper(dbfile) {}
-        
+        BindingMapper(const std::string &dbfile) : _sql_helper(dbfile)
+        {
+            std::string path = FileHelper::parentDirectory(dbfile);
+            FileHelper::createDirectory(path);
+            _sql_helper.open();
+            createTable();
+        }
+
         // 创建数据库表
-        void createTable();
-        
+        void createTable()
+        {
+            // create table if not exists binding_table(exchange_name varchar(32), msgqueue_name, binding_key)
+            std::stringstream sql;
+            sql << "create table if not exists binding_table(";
+            sql << "exchange_name varchar(32), ";
+            sql << "msgqueue_name varchar(32), ";
+            sql << "binding_key varchar(128));";
+            assert(_sql_helper.exec(sql.str(), nullptr, nullptr));
+        }
+
         // 删除数据库表
-        void removeTable();
+        void removeTable()
+        {
+            std::string sql = "drop table if exists binding_table;";
+            _sql_helper.exec(sql, nullptr, nullptr);
+        }
 
         // 添加绑定信息
-        bool insert(Binding::ptr &binding);
+        bool insert(Binding::ptr &binding)
+        {
+            // insert into binding_table values('exchange1', 'msgqueue1', 'news.music.#');
+            std::stringstream sql;
+            sql << "insert into binding_table values(";
+            sql << "'" << binding->exchange_name << "', ";
+            sql << "'" << binding->msgqueue_name << "', ";
+            sql << "'" << binding->binding_key << "');";
+            return _sql_helper.exec(sql.str(), nullptr, nullptr);
+        }
 
         // 移除绑定信息
-        void remove(const std::string &ename, const std::string &qname);
-        
+        void remove(const std::string &ename, const std::string &qname)
+        {
+            // delete form binding_table where exchange_name='' and msgqueue_name='';
+            std::stringstream sql;
+            sql << "delete from binding_table where ";
+            sql << "exchange_name='" << ename << "' and ";
+            sql << "msgqueue_name='" << qname << "';";
+            _sql_helper.exec(sql.str(), nullptr, nullptr);
+        }
+
         // 移除交换机绑定信息
-        void removeExchangeBindings(const std::string &ename);
+        void removeExchangeBindings(const std::string &ename)
+        {
+            // delete from binding_table where exchange_name='';
+            std::stringstream sql;
+            sql << "delete from binding_table where ";
+            sql << "exchange_name='" << ename << "';";
+            _sql_helper.exec(sql.str(), nullptr, nullptr);
+        }
 
         // 移除队列绑定信息
-        void removeMsgQueueBindings(const std::string &qname);
+        void removeMsgQueueBindings(const std::string &qname)
+        {
+            std::stringstream sql;
+            sql << "delete from binding_table where ";
+            sql << "msgqueue_name='" << qname << "';";
+            _sql_helper.exec(sql.str(), nullptr, nullptr);
+        }
 
         // 获取所有数据(恢复历史数据)
-        BindingMap recovery();
+        BindingMap recovery()
+        {
+            BindingMap result;
+            std::string sql = "select exchange_name, msgqueue_name, binding_key from binding_table;";
+            _sql_helper.exec(sql, selectCallback, &result);
+            return result;
+        }
+
+    private:
+        static int selectCallback(void *arg, int numcol, char **row, char **fields)
+        {
+            BindingMap *result = (BindingMap *)arg;
+            Binding::ptr bp = std::make_shared<Binding>(row[0], row[1], row[2]);
+            // 为了防止 交换机相关的绑定信息已经存在，因此不能直接创建队列映射 进行添加 这样会覆盖历史数据
+            // 因此得先获取交换机对应的映射对象 往里面添加数据
+            // 但是 若这时候没有交换机对应的映射信息 因此这里的获取要使用引用（会保证不存在则自动创建）
+            MsgQueueBindingMap &qmap = (*result)[bp->exchange_name];
+            qmap.insert(std::make_pair(bp->msgqueue_name, bp));
+            return 0;
+        }
+
     private:
         SqliteHelper _sql_helper;
     };
