85a45556f170a26cb43b0cdb67010060e211cea8
Hao_Ping <2429302207@qq.com>
2026-06-30 22:40:23 +0800

    虚拟机管理类——交换机相关操作实现

--- mqserver/mq_host.hpp
+++ mqserver/mq_host.hpp
@@ -8,17 +8,41 @@
 
 namespace haoping
 {
+    // 虚拟主机管理类
     class VirtualHost
     {
     public:
-        VirtualHost(const std::string &basedir, const std::string &dbfile);
+        VirtualHost(const std::string const host_name, std::string &basedir, const std::string &dbfile)
+            : _host_name(host_name),
+              _emp(std::make_shared<ExchangeManager>(dbfile)),
+              _mqmp(std::make_shared<MsgQueueManager>(dbfile)),
+              _bmp(std::make_shared<BindingManager>(dbfile)),
+              _mmp(std::make_shared<MessageManager>(basedir))
+        {
+            //获取到所有的队列信息，通过队列名称恢复历史消息数据
+            QueueMap qm = _mqmp->allQueues();
+            for(auto &q : qm)
+            {
+                _mmp->initQueueMessage(q.first);
+            }
+            
+        }
 
         // 声明交换机
         bool declareExchange(const std::string &name,
                              ExchangeType type, bool durable, bool auto_delete,
-                             std::unordered_map<std::string, std::string> &args);
+                             std::unordered_map<std::string, std::string> &args)
+        {
+            return _emp->declareExchange(name, type, durable, auto_delete, args);
+        }
+
         // 删除交换机
-        void deleteExchange(const std::string &name);
+        void deleteExchange(const std::string &name)
+        {
+            //删除交换机的时候，需要将交换机相关的绑定信息也删除掉。
+            _bmp->removeExchangeBindings(name);
+            return _emp->deleteExchange(name);
+        }
 
         // 声明队列
         bool declareQueue(const std::string &qname,
